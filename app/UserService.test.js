4d044a81f4d63a72469068c732e5a434524252ce
Al <alistairwoodcock@gmail.com>
2018-09-22 00:15:15 +1000

    fixed tests after interface change

--- app/UserService.test.js
+++ app/UserService.test.js
@@ -20,9 +20,14 @@ describe("User Payments", () => {
     let repo;
     let userService;
 
+    let userA;
+    let userB;
+
     beforeEach(() => {
         repo = new MockUserRepo();
         repo.users = [{id: 0, name: "A", balance: 10}, {id: 1, name: "B", balance: 0}];
+        userA = repo.getUserById(0)
+        userB = repo.getUserById(1)
         userService = new UserService(repo);
     })
     
@@ -32,10 +37,7 @@ describe("User Payments", () => {
 
     test("Pays user", () => {
         
-        userService.makePayment(0, 1, 10);
-
-        let userA = repo.getUserById(0);
-        let userB = repo.getUserById(1);
+        userService.makePayment(userA, userB, 10);
 
         expect(userA.balance).toEqual(0);
         expect(userB.balance).toEqual(10);
@@ -43,50 +45,41 @@ describe("User Payments", () => {
 
     test("Cannot pays user more than balance", () => {
         
-        let result = userService.makePayment(0, 1, 20);
+        let [success, reason] = userService.makePayment(userA, userB, 20);
 
-        let userA = repo.getUserById(0);
-        let userB = repo.getUserById(1);
-
-        expect(result).toEqual(false);
+        expect(success).toEqual(false);
         expect(userA.balance).toEqual(10);
     });
 
     test("Cannot pay nonexistent user", () => {
-        let result = userService.makePayment(0, -1, 10);
+        let [success, reason] = userService.makePayment(userA, undefined, 10);
 
-        let userA = repo.getUserById(0);
-
-        expect(result).toEqual(false);
+        expect(success).toEqual(false);
         expect(userA.balance).toEqual(10);
     });
 
     test("Cannot pay from nonexistent user", () => {
-        let result = userService.makePayment(-1, 1, 10);
+        let [success, reason] = userService.makePayment(undefined, userB, 10);
 
-        let userB = repo.getUserById(1);
-
-        expect(result).toEqual(false);
+        expect(success).toEqual(false);
         expect(userB.balance).toEqual(0);
     });
 
     test("Cannot pay negative balance", () => {
-        let result = userService.makePayment(0, 1, -10);
+        let [success, reason] = userService.makePayment(userA, userB, -10);
 
-        let userB = repo.getUserById(1);
-
-        expect(result).toEqual(false);
+        expect(success).toEqual(false);
         expect(userB.balance).toEqual(0);
     });
 
     test("Cannot pay zero", () => {
-        let result = userService.makePayment(0, 1, 0);
-        expect(result).toEqual(false);
+        let [success, reason] = userService.makePayment(userA, userB, 0);
+        expect(success).toEqual(false);
     })
 
     test("Connect pay to and from same user", () => {
-        let result = userService.makePayment(0, 1, 0);        
-        expect(result).toEqual(false);
+        let [success, reason] = userService.makePayment(userA, userB, 0);        
+        expect(success).toEqual(false);
     })
 
 });
